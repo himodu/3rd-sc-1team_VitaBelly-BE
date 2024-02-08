512a11aee90dfff5ef0c89df6ba27afc2dd5f4b6
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-02-08 15:49:15 +0900

    프로젝트 세팅
    자바버전: 17, 스프링부트 : 3.2.2

---
+++ src/main/java/GDSCKNU/VitaBelly/VitaBellyApplication.java
@@ -0,0 +1,13 @@
+package GDSCKNU.VitaBelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaBellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaBellyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSCKNU/VitaBelly/VitaBellyApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSCKNU.VitaBelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaBellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
