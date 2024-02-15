ab5c3b582edab2f661db30c8772a476bebc4dc30
Seokhun-Yang <ddayang1234@gmail.com>
2024-02-15 17:36:21 +0900

    Clinic model 추가

---
+++ src/main/java/GDSCKNU/VitaBelly/dto/Location.java
@@ -0,0 +1,2 @@
+package GDSCKNU.VitaBelly.dto;public class Location {
+}
---
+++ src/main/java/GDSCKNU/VitaBelly/model/Clinic.java
@@ -0,0 +1,11 @@
+package GDSCKNU.VitaBelly.model;
+
+import lombok.Data;
+
+@Data
+public class Clinic {
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+}
